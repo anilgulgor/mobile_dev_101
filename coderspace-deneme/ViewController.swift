209ec18394b61a0bda55b6a5e181511c3254159b
angulgor <anilgul@migros.com.tr>
2024-05-03 11:13:06 +0300

    Initial Commit

---
+++ coderspace-deneme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coderspace-deneme
+//
+//  Created by ANIL GULGOR on 3.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coderspace-denemeUITests/coderspace_denemeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  coderspace_denemeUITestsLaunchTests.swift
+//  coderspace-denemeUITests
+//
+//  Created by ANIL GULGOR on 3.05.2024.
+//
+
+import XCTest
+
+final class coderspace_denemeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
